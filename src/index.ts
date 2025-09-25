28bc4ee709d62d33a4d35c882d3769412de6539f
alexglow2000-design <alexglow2000@gmail.com>
2025-09-25 20:42:31 +0300

    не знаю какой уже коммит3

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,12 @@ import express from "express";
 import routes from './routes/index'
 import { errorHandler } from "./middlewares/errorHandler";
 import cors from 'cors'
-import fileUpload from 'express-fileupload'
 import path from "path";
 
 const app = express();
 
 app.use(cors())
 app.use(express.json());
-app.use(fileUpload({}))
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use('/api', routes);
 app.use(errorHandler);
