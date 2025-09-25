26042c01dce3c0667d2fc7c37c9345ba7b0e9366
alexglow2000-design <alexglow2000@gmail.com>
2025-09-25 21:16:00 +0300

    не знаю какой уже коммит15

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,16 @@ import sequelize from './db'
 
 const start = async () => {
     try {
-        await sequelize.authenticate()
-        await sequelize.sync()
-        app.listen(config.port,  async () => {
+        await sequelize.authenticate();
+        await sequelize.sync();
+
+        app.listen(config.port, () => {
             console.log(`Server running on port ${config.port}`);
         });
     } catch (e) {
-        console.log(e)
+        console.error("Failed to start server:", e);
+        process.exit(1); // важно, чтобы Render видел ошибку
     }
 }
 
-start()
+start();
